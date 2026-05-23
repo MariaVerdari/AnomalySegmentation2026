bac25f191eff21228c445f6c2a35b17d3deb6d5a
MariaVerdari <s310724@studenti.polito.it>
2026-05-23 11:49:41 +0200

    sfasamento label erfnet cityscapes

--- eval/eval_iou.py
+++ eval/eval_iou.py
@@ -26,6 +26,28 @@ from iouEval import iouEval, getColorEntry
 NUM_CHANNELS = 3
 NUM_CLASSES = 20
 
+# blocco aggiunto per le etichette sfasate 
+
+class MapToTrainIds(object):
+    def __init__(self):
+        mapping = {
+            0: 255, 1: 255, 2: 255, 3: 255, 4: 255, 5: 255, 6: 255, 7: 0, 8: 1, 9: 255,
+            10: 255, 11: 2, 12: 3, 13: 4, 14: 255, 15: 255, 16: 255, 17: 5, 18: 255,
+            19: 6, 20: 7, 21: 8, 22: 9, 23: 10, 24: 11, 25: 12, 26: 13, 27: 14, 28: 15,
+            29: 255, 30: 255, 31: 16, 32: 17, 33: 18, -1: 255
+        }
+        self.lut = np.zeros(256, dtype=np.uint8)
+        for k, v in mapping.items():
+            if k >= 0:
+                self.lut[k] = v
+
+    def __call__(self, img):
+        img_np = np.array(img)
+        mapped_np = self.lut[img_np]
+        return Image.fromarray(mapped_np)
+    
+
+
 image_transform = ToPILImage()
 input_transform_cityscapes = Compose([
     Resize(512, Image.BILINEAR),
@@ -33,6 +55,7 @@ input_transform_cityscapes = Compose([
 ])
 target_transform_cityscapes = Compose([
     Resize(512, Image.NEAREST),
+    MapToTrainIds(),
     ToLabel(),
     Relabel(255, 19),   #ignore label to 19
 ])
