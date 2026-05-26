79814e2b0036c9ef9e61a42a9d657f3874433d7e
MariaVerdari <s310724@studenti.polito.it>
2026-05-26 12:51:26 +0200

    relabel

--- eval/eval_iou.py
+++ eval/eval_iou.py
@@ -79,7 +79,7 @@ target_transform_cityscapes = Compose([
     Resize(512, Image.NEAREST),
     #MapToTrainIds(),
     ToLabel(),
-    #Relabel(255, 19),   #ignore label to 19
+    Relabel(255, 19),   #ignore label to 19
 ])
 
 def main(args):
